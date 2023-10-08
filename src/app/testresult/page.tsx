7d94782459c875e0d848a8d69ba3f5fa85620c3f
sonyujin <jainayj804998@gmail.com>
2023-10-09 00:52:58 +0900

    Added testresult/page.tsx : add TypeTest comoponent

--- src/app/testresult/page.tsx
+++ src/app/testresult/page.tsx
@@ -1,7 +1,7 @@
-export default function TypeTest() {
+export default function TestResult() {
     return (
         <main className="flex h-full w-full flex-col items-center justify-between p-24">
-            <p>This is Dormitory Life Type Test page</p>
+            <p>This is Dormitory Life Type Test Result page</p>
         </main>
     )
 }
