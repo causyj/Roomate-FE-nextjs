f8b3aa951dda81994204e6350ebdec1ad4a02bd8
sonyujin <jainayj804998@gmail.com>
2023-10-09 01:15:28 +0900

    Update recommendationresult : add links for example

--- src/app/recommendationresult/page.tsx
+++ src/app/recommendationresult/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function RecommendationResult() {
     return (
         <main className="flex h-full w-full flex-col items-center justify-between p-24">
             <p>This is Roomate Recommendation page</p>
+            <Link href="/recommendationresult/1">recommendationresult 1</Link>
+            <Link href="/recommendationresult/2">recommendationresult 2</Link>
         </main>
     )
 }
