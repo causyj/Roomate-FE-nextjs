e681e7c4cf4dd0dc851c67b7692156d51ac2f0c6
sonyujin <jainayj804998@gmail.com>
2023-10-09 00:34:14 +0900

    Update layout.tss : Move Links from page to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import Image from 'next/image'
-const Links = () => {
-  return (
-    <>
-      <p>Links</p>
-    </>
-  )
-}
+
 export default function Home() {
   return (
     //min-h-screen
     <main className="flex h-full w-full flex-col items-center justify-between p-24">
-      <Links />
-      <Links />
+      <div>test</div>
     </main>
   )
 }
