7750fa15c151a74aee9a07aeba5a1b5b380d548c
sonyujin <jainayj804998@gmail.com>
2023-10-09 00:43:03 +0900

    Added Login Compoonent

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <main className="flex h-full w-full flex-col items-center p-24">
+            <p>This is Login page</p>
+            
+        </main>
+    )
+}
