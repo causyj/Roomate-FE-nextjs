4352c26dfca4c65abff959a17f7449f5d5f7b041
sonyujin <jainayj804998@gmail.com>
2023-10-09 00:50:19 +0900

    Added typetest/page.tsx : add TypeTest Component

---
+++ src/app/typetest/page.tsx
@@ -0,0 +1,7 @@
+export default function TypeTest() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Dormitory Life Type Test</p>
+        </main>
+    )
+}
