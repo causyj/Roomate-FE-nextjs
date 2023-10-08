ab619228e04edc1f4258836f72ca5858714b631a
sonyujin <jainayj804998@gmail.com>
2023-10-09 00:55:42 +0900

    Added TypeList Component

---
+++ src/app/type/page.tsx
@@ -0,0 +1,7 @@
+export default function TypeList() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Dormitory Life Type List page</p>
+        </main>
+    )
+}
