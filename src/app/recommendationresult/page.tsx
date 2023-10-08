4c2bdb6b0922d2d2205d134084ce9ca6f27e8574
sonyujin <jainayj804998@gmail.com>
2023-10-09 01:04:35 +0900

    Added RecommendationResult Component

---
+++ src/app/recommendationresult/page.tsx
@@ -0,0 +1,7 @@
+export default function RecommendationResult() {
+    return (
+        <main className="flex h-full w-full flex-col items-center justify-between p-24">
+            <p>This is Roomate Recommendation page</p>
+        </main>
+    )
+}
