9ce19ab2bb353bf8136c3d95e4cc548c0adc8399
sonyujin <jainayj804998@gmail.com>
2023-10-09 01:16:40 +0900

    Update type : add links for example

--- src/app/type/page.tsx
+++ src/app/type/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 export default function TypeList() {
     return (
         <main className="flex h-full w-full flex-col items-center justify-between p-24">
             <p>This is Dormitory Life Type List page</p>
+            <Link href="/type/1">type 1</Link>
+            <Link href="/type/2">type 2</Link>
+        
         </main>
     )
 }
