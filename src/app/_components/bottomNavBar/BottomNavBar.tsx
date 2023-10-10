ff391ddad4936130c50e409dca2398d5c642e71a
sonyujin <jainayj804998@gmail.com>
2023-10-10 17:42:13 +0900

    Added BottomNavBar Componenet

--- src/app/_components/bottomNavBar/BottomNavBar.tsx
+++ src/app/_components/bottomNavBar/BottomNavBar.tsx
@@ -1,28 +1,46 @@
 "use client"
-import React from 'react';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+import Paper from '@mui/material/Paper';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Home from '@mui/icons-material/HomeRounded';
 import Chat from '@mui/icons-material/ChatRounded'
 import Notice from '@mui/icons-material/NotificationsRounded';
+
+
 export const BottomNavBar = () => {
-    const [value, setValue] = React.useState(0);
-    const [isOpened, setIsOpened] = useState(false) // outline or not 
+    const [value, setValue] =useState(0);
+ 
+    const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
+    //const [value, setValue] = React.useState(pathname);
+    
+    useEffect(() => {
+        if (pathname === '/') {
+          setValue(0);
+        } else if (pathname === '/type') {
+          setValue(1);
+        } else if (pathname === '/login') {
+          setValue(2);
+        }
+      }, [pathname]);
     return (
-        <div style={{ position: 'fixed', bottom: 0, width: '100%'} }>
-            <BottomNavigation
-            showLabels
-            value={"home"}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            >
-            <BottomNavigationAction label="Recents" icon={<Home />} />
-            <BottomNavigationAction label="Favorites" icon={<Chat />} />
-            <BottomNavigationAction label="Nearby" icon={<Notice />} />
-            </BottomNavigation>
-        </div>
+         <Paper style={{ position: 'fixed', bottom: 0, width: '100%'} }>
+    
+        <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+            setValue(newValue);
+            
+        }}>
+        <BottomNavigationAction href='/' label="Home" icon={<Home />}></BottomNavigationAction> 
+        <BottomNavigationAction href='/type' label="Chat" icon={<Chat />}></BottomNavigationAction> 
+        <BottomNavigationAction href='/login' label="Notice" icon={<Notice />}></BottomNavigationAction>
+        </BottomNavigation>
+          
+        </Paper>
        
     )
-}
+    
+    }
